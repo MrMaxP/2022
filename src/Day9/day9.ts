e5f413bf53d4cc3fac0a96f9df4de59e99972f00
MrMaxP <stuart@slanty.com>
2022-12-14 00:42:49 +0000

    Day9

--- src/Day9/day9.ts
+++ src/Day9/day9.ts
@@ -22,12 +22,113 @@ export class Day9
 
 	private Part1(): number
 	{
-		return 0;
+		const bound: Bound = this.getBounds();
+		const grid = new Grid(bound);
+		grid.simulate();
+
+		const count: number = grid.countUsed();
+
+		console.log("Spaces visited ", count);
+
+		return count;
 	}
 
 	private Part2(): number
 	{
 		return 0;
 	}
+
+	private getBounds(): Bound
+	{
+		let bound: Bound = new Bound();
+
+		let x: number = 0;
+		let y: number = 0;
+
+	for(const line of this.lines)
+		{
+			switch(line.charAt(0))
+			{
+				case 'U': y += Number.parseInt(line.substring(2)); break;
+				case 'D': y -= Number.parseInt(line.substring(2)); break;
+				case 'L': x -= Number.parseInt(line.substring(2)); break;
+				case 'R': x += Number.parseInt(line.substring(2)); break;
+			}
+
+			console.log(line);
+
+			bound.expand(x, y);
+		}
+
+		return bound;
+	}
 }
 
+class Bound
+{
+	r: number = 0;
+	l: number = 0;
+	t: number = 0;
+	b: number = 0;
+
+	expand(x: number, y: number)
+	{
+		this.r = Math.max(this.r, x);
+		this.l = Math.min(this.l, x);
+
+		this.t = Math.max(this.t, y);
+		this.b = Math.min(this.b, y);
+
+		console.log(`Bound expanded to L:${this.l}, R:${this.r}, T:${this.t}, B:${this.b}`)
+	}
+}
+
+class Grid
+{
+	x: number = 0;
+	y: number = 0;
+	w: number = 0;
+	h: number = 0;
+	grid: number[][] = [];
+
+	constructor(bound: Bound)
+	{
+		this.w = bound.r - bound.l;
+		this.h = bound.t - bound.b;
+
+		this.x = -bound.l;
+		this.y = -bound.b;
+
+		for(let i: number = 0; i < this.h; i++)
+		{
+			this.grid[i] = [];
+			for(let j: number = 0; j < this.w; j++)
+			{
+				this.grid[i][j] = 0;
+			}
+		}
+	}
+
+	simulate()
+	{
+
+	}
+
+	countUsed(): number
+	{
+		let count: number = 0;
+
+		for(let x: number = 0; x < this.w; x++)
+		{
+			for(let y: number = 0; y < this.h; y++)
+			{
+				if(this.grid[y][x] !== 0)
+				{
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+}
